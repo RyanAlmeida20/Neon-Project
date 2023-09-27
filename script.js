1d6c0767b2fc483c600642b6aa461b939fe329d1
RyanAlmeida20 <ryluz03@hotmail.com>
2023-09-27 00:32:48 -0300

    Updating some things

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 // DROPDOWN
 
-const btnDropdown = document.querySelector('.js-btn-dropdown');
-const dropdown = document.querySelector('.js-dropdown');
+const btnDropdown = document.querySelector(".js-btn-dropdown");
+const dropdown = document.querySelector(".js-dropdown");
 
-function openDropdown(event){
-    event.preventDefault();
+function openDropdown(event) {
+  event.preventDefault();
 
-    dropdown.classList.toggle('active');
+  dropdown.classList.toggle("active");
 }
 
-btnDropdown.addEventListener('click', openDropdown);   
+btnDropdown.addEventListener("click", openDropdown);
 
-dropdown.addEventListener('mouseleave', openDropdown);  
+dropdown.addEventListener("mouseleave", openDropdown);
 
 // Fixed Menu
 
-const header = document.getElementById('js-header')
+const header = document.getElementById("js-header");
 
 function fixedMenu() {
-    if(window.pageYOffset > 80) {
-        header.classList.add('fixed-menu');
-    } else{
-        header.classList.remove('fixed-menu')
-    }
+  if (window.pageYOffset > 719) {
+    header.classList.add("fixed-menu");
+  } else {
+    header.classList.remove("fixed-menu");
+  }
 }
 
-document.addEventListener('scroll', fixedMenu);
+console.log(window.scrollY);
+
+document.addEventListener("scroll", fixedMenu);
 
 // Menu Mobile
 
-const btnMenu = document.getElementById('js-btn-menu-mobile');
-const overlayMenu = document.querySelector('.js-overlay');
+const btnMenu = document.getElementById("js-btn-menu-mobile");
+const overlayMenu = document.querySelector(".js-overlay");
 
-function openMenuMobile (){
-    document.documentElement.classList.toggle('menu-opened')
+function openMenuMobile() {
+  document.documentElement.classList.toggle("menu-opened");
 }
 
-btnMenu.addEventListener('click', openMenuMobile)
+btnMenu.addEventListener("click", openMenuMobile);
 
-overlayMenu.addEventListener('click', openMenuMobile)
+overlayMenu.addEventListener("click", openMenuMobile);
