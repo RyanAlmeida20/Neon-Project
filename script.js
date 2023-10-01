bd4bfad49abff92892e01db544353d39250335a4
RyanAlmeida20 <ryluz03@hotmail.com>
2023-09-30 23:09:34 -0300

    Changed the elements' order in the first section

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// DROPDOWN
-
-// const btnDropdown = document.querySelector(".js-btn-dropdown");
-// const dropdown = document.querySelector(".js-dropdown");
-
-// function openDropdown(event) {
-//   event.preventDefault();
-
-//   dropdown.classList.toggle("active");
-// }
-
-// btnDropdown.addEventListener("click", openDropdown);
-
-// dropdown.addEventListener("mouseleave", openDropdown);
-
 // Fixed Menu
 
 const header = document.getElementById("js-header");
@@ -25,19 +10,4 @@ function fixedMenu() {
   }
 }
 
-console.log(window.scrollY);
-
 document.addEventListener("scroll", fixedMenu);
-
-// Menu Mobile
-
-const btnMenu = document.getElementById("js-btn-menu-mobile");
-const overlayMenu = document.querySelector(".js-overlay");
-
-function openMenuMobile() {
-  document.documentElement.classList.toggle("menu-opened");
-}
-
-btnMenu.addEventListener("click", openMenuMobile);
-
-// overlayMenu.addEventListener("click", openMenuMobile);
